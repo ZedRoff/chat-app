1d8c0ad4f6ed3e25a4ae54b2f42404db2e82b783
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-08-09 23:14:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 5500 })
+const express = require("express")
+const app = express()
+const config = require("./public/config.json")
+const server = app.listen(5000, config.ipv4)
+app.use(express.static("public"))
+const wss = new WebSocket.Server({ server })
 
 let users = []
 let messages = []
 let i = 0;
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+app.get("/ipv4", (req, res) => {
+    res.send(config.ipv4)
+})
 wss.on("connection", ws => {
     users.push(ws)
     i++
